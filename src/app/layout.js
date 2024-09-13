b88ea8cdf21a0bb084f5a7a616be929b8f1f10ef
erika komori <komoriakirarika@erikaMBA.local>
2024-09-13 10:57:59 +0900

    OGC画像の追加

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
 import './globals.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head'; // Headコンポーネントをインポート
 
 export const metadata = {
-  title: "mysterious bunny",
+  title: "Mysterious Bunny",
   description: "The Bunny Offers Relationship Tips",
 };
 
@@ -11,8 +12,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-         {/* 左上のロゴ画像 */}
-         <div className="absolute top-0 left-0 p-4 z-20">
+        <Head>
+          {/* Open Graphメタタグ */}
+          <meta property="og:title" content="Mysterious Bunny" />
+          <meta property="og:description" content="恋愛系マスターおもちの相談室" />
+          <meta property="og:image" content="https://mysterious-bunny.vercel.app/ogc_image.png" />
+          <meta property="og:url" content="https://mysterious-bunny.vercel.app/" />
+          <meta property="og:type" content="website" />
+
+          {/* Twitterカードメタタグ */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Mysterious Bunny" />
+          <meta name="twitter:description" content="恋愛系マスターおもちの相談室" />
+          <meta name="twitter:image" content="https://mysterious-bunny.vercel.app/ogc_image.png" />
+          <meta name="twitter:url" content="https://mysterious-bunny.vercel.app/" />
+        </Head>
+
+        {/* 左上のロゴ画像 */}
+        <div className="absolute top-0 left-0 p-4 z-20">
           <Link href="/" passHref>
             <Image
               src="/title_logo.png"
@@ -25,7 +42,7 @@ export default function RootLayout({ children }) {
           </Link>
         </div>
 
-         {/* ページの内容 */}
+        {/* ページの内容 */}
         <div className="flex flex-col min-h-screen">
           {children}
         </div>
