bbda68a82c064bf96a27355cb0661b117b32a1b0
erika komori <komoriakirarika@erikaMBA.local>
2024-09-13 13:50:27 +0900

    OGC画像の追加

--- src/app/omochi_bot/page.jsx
+++ src/app/omochi_bot/page.jsx
@@ -22,8 +22,8 @@ export default function OmochiLoveBot() {
           {/* Card 1 */}
           <div className="bg-[rgba(255,255,255,0.5)] bg-opacity-90 p-6 rounded-xl shadow-md text-center">
             <Image src="/omochi04.png" width={150} height={150} alt="Rabbit 1" className="mx-auto mb-4" />
-            <h2 className="font-bold mb-2">おもちの恋愛クイズ</h2>
-            <p>
+            <h2 className="font-bold mb-2 text-[#645345]">おもちの恋愛クイズ</h2>
+            <p className="font-bold mb-2 text-[#645345]">
               おもちの恋愛観や気をつけていることを、クイズにしたでちゅ。人間にも共通することがあるかもしれないから、みんなに教えちゃうでちゅ。
             </p>
             <button
@@ -37,8 +37,8 @@ export default function OmochiLoveBot() {
           {/* Card 2 */}
           <div className="bg-[rgba(255,255,255,0.5)] bg-opacity-90 p-6 rounded-xl shadow-md text-center">
             <Image src="/omochi05.png" width={150} height={150} alt="Rabbit 2" className="mx-auto mb-4" />
-            <h2 className="font-bold mb-2">モテるテクニック</h2>
-            <p>おもち直伝！おもちの可愛さと、おもちがかわいい理由を伝授するでちゅ。</p>
+            <h2 className="font-bold mb-2 text-[#645345]">モテるテクニック</h2>
+            <p className="font-bold mb-2 text-[#645345]">おもち直伝！おもちの可愛さと、おもちがかわいい理由を伝授するでちゅ。</p>
             <button
               onClick={() => alert('リンク先はまだ準備中です')}
               className="mt-4 px-4 py-2 bg-[#cb8e7e] text-white rounded-lg shadow-lg hover:bg-[#b77a6b] transition-all"
