ee5073097067a34bddca94db6941eeb9e8d7d94b
erika komori <komoriakirarika@erikaMBA.local>
2024-09-14 17:31:56 +0900

    googleアナリティクスを導入

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,14 +1,14 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // ページビューのトラッキング
-export const pageview = (url: string) => {
+export const pageview = (url) => {
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
 // イベントのトラッキング
-export const event = ({ action, category, label, value }: { action: string; category: string; label: string; value: string; }) => {
+export const event = ({ action, category, label, value }) => {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
